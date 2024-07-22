a58f0d4d2c254d0a2d2fd73b4e4922d0d1718d81
Syahril Haryono <syhrlhyn@gmail.com>
2024-07-22 18:13:50 +0700

    Descriptive message about your changes

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <Nuxt />
+    <!-- navbar -->
+    <Navbar />
+    <div class="mt-20 sm:mb-0 mb-22">
+      <!-- content -->
+      <Nuxt />
+    </div>
+
+
+    <!-- footer -->
+    <Footer />
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/web/Navbar.vue'
+  import Footer from '@/components/web/Footer.vue'
 export default {
+
   data() {
     return {
       image: null
