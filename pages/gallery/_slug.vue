bf43855dc6d61fb4baa72f10aab7f6b7c5ced450
Syahril Haryono <syhrlhyn@gmail.com>
2024-07-22 18:39:35 +0700

    Descriptive message about your changes

--- pages/gallery/_slug.vue
+++ pages/gallery/_slug.vue
@@ -19,7 +19,7 @@
             <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 9l4-4-4-4"/>
             </svg>
-            <NuxtLink v-if="galleries" :to="{ name: 'galleries-slug', params: { slug: galleries.slug } }" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">{{ galleries.title }}</NuxtLink>
+            <NuxtLink v-if="galleries" :to="{ name: 'gallery-slug', params: { slug: gallery.slug } }" class="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">{{ galleries.title }}</NuxtLink>
           </li>
         </ol>
       </nav>
